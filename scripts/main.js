19ade598524b461113b12278978616887a2f8588
racedstar <racedstar@gmail.com>
2018-03-04 00:11:34 +0800

    Add All

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$.getJSON('./Mp05001.json',function(data){    
+    // var keys = Object.getOwnPropertyNames(data[0]);
+    // for(var i=0;i<data.length; i++){
+    //     // if(data[i][key[0]].indexOf('M') == -1){
+    //     //     var array =[];
+            
+    //     // }
+    //     for(var j=0; j < data[i].length; i++){
+    //          console.log(j);
+    //     }
+        
+    // }
+})
